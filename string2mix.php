808d98d8804a3edca690f04d3bf4e054d46c166f
k22pr <adm@an1.kr>
2017-02-02 10:48:58 +0900

    예제첨부 / simple_hash함수 생성
    상세한 예제 설명과 이미지첨부 및 simple_hash제작

--- string2mix.php
+++ string2mix.php
@@ -4,6 +4,8 @@
 	
 	// 2개 이상의 문자열을 서로 섞어 줍니다.
 	//1글자의 배열들은 섞이지 않습니다.
+	function strmix(array $str) : string {return string2mix($str);}
+	function str2mix(array $str) : string {return string2mix($str);}
 	function string2mix(array $str) : string{
 		$value = (string) NULL;
 		if(count($str) == 1) return "string2mix say : One array can not mix!";
@@ -30,18 +32,36 @@
 			return $value;
 		}
 	}
+	
+	
+	//사용 예제
+	//rainbow table방지를 위한 안전한 해쉬 생성 / 생성시 무작위 값을 넣으면 안됩니다! (당연한 거지만...)
+	function make_hash(string $pass,$type="sha512") : string{
+		$type = strtolower(trim($type));
+		$arr = array(NULL,NULL);
+		for($i = 0;$i<mb_strlen($pass,"UTF-8");$i++){
+			$base = ord(mb_substr($pass, $i, 1, "UTF-8" ));
+			$arr[0] .= $base;
+			$arr[1] .= strrev($base);
+		}
+		return hash($type,string2mix($arr));
+	}
+	function simple_hash(string $pass) : string{
+		return md5(string2mix([md5(md5($pass)),strrev(md5($pass))]));
+	}
+	
+	
+	//example list
 	echo string2mix(["hello"])."<br>";
 	echo string2mix(["hello","  "])."<br>";
 	echo string2mix(["hello","world"])."<br>";
 	echo string2mix([2,4,8,16,32,64])."<br>";
 	echo string2mix(["HTML5","PHP7","CSS3","SCSS","jQuery","ANIA"])."<br>";
-	echo string2mix(["이걸 왜 만들고 있는 걸까?","그걸 왜 나한테 물어봐ㅠ"])."<br>";
-
-
-	function make_hash(string $pass,$type="sha512") : string{
-		$type = strtolower(trim($type));
-		return hash($type,string2mix([$pass,hash($type,$pass)]));
-	}
+	echo string2mix(["비밀번호","안알랴줌"])."<br>";
+	
 	$pass = "password";
-	echo make_hash($pass,"md5")."<br>";
+	
+	echo "make_hash() :  ".make_hash($pass)."<br>";
+	echo "simple_hash() :  ".simple_hash($pass)."<br>";
+	
 ?>
