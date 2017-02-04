f151d899c8b75e2085e9a820e5163d8cf1d06445
k22pr <adm@an1.kr>
2017-02-05 00:41:59 +0900

    make_hash update

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	// Make By K22pr - 2017-02-02
+	// https://github.com/k22pr/PHPx-String2Mix
+	
+	// 2개 이상의 문자열을 서로 섞어 줍니다.
+	//1글자의 배열들은 섞이지 않습니다.
+	require_once("./source.php");
+	
+	//example list
+	echo string2mix(["hello"])."<br>";
+	echo string2mix(["hello","  "])."<br>";
+	echo string2mix(["hello","world"])."<br>";
+	echo string2mix([2,4,8,16,32,64])."<br>";
+	echo string2mix(["HTML5","PHP7","CSS3","SCSS","jQuery","ANIA"])."<br>";
+	echo string2mix(["비밀번호","안알랴줌"])."<br>";
+	
+	$pass = "패스word";
+	
+	echo "make_hash() :  ".make_hash($pass)."<br>";
+	echo "simple_hash() :  ".simple_hash($pass)."<br>";
+	
+?>
--- source.php
+++ source.php
@@ -38,30 +38,27 @@
 	//rainbow table방지를 위한 안전한 해쉬 생성 / 생성시 무작위 값을 넣으면 안됩니다! (당연한 거지만...)
 	function make_hash(string $pass,$type="sha512") : string{
 		$type = strtolower(trim($type));
-		$arr = array(NULL,NULL);
+		$arr = array();
+		$bin = NULL;
+		$hex = NULL;
 		for($i = 0;$i<mb_strlen($pass,"UTF-8");$i++){
-			$base = ord(mb_substr($pass, $i, 1, "UTF-8" ));
-			$arr[0] .= $base;
-			$arr[1] .= strrev($base);
+			$arr[] = mb_substr($pass, $i, 1, "UTF-8" );
 		}
-		return hash($type,string2mix($arr));
+		foreach($arr as $no => $list){
+			$hex .= base_convert(bin2hex($list),16,2);
+		}
+		
+		for($i=0;$i < 4;$i++){
+			$size = floor(strlen($hex/4));
+			$ceil[] = substr($hex,$size*$i,$size);
+			if($i%2 == 1) $str[] = (hash($type,$ceil[$i]) ^ hash($type,$ceil[$i-1]));
+		}
+		
+		return hash($type,string2mix([$str[0],$str[1]]));
 	}
 	function simple_hash(string $pass) : string{
 		return md5(string2mix([md5(md5($pass)),strrev(md5($pass))]));
 	}
 	
-	
-	//example list
-	echo string2mix(["hello"])."<br>";
-	echo string2mix(["hello","  "])."<br>";
-	echo string2mix(["hello","world"])."<br>";
-	echo string2mix([2,4,8,16,32,64])."<br>";
-	echo string2mix(["HTML5","PHP7","CSS3","SCSS","jQuery","ANIA"])."<br>";
-	echo string2mix(["비밀번호","안알랴줌"])."<br>";
-	
-	$pass = "password";
-	
-	echo "make_hash() :  ".make_hash($pass)."<br>";
-	echo "simple_hash() :  ".simple_hash($pass)."<br>";
 	
 ?>
